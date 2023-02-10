2dea100cc05804f96ef2a865d097b72cedc72cb5
fabian <f.soelle@lmg-schulen-willich.de>
2023-02-10 12:04:09 +0100

    Neu

---
+++ tilesets_test/MegaPixelArt32x32pxIcons_SpriteSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="MegaPixelArt32x32pxIcons_SpriteSheet" tilewidth="32" tileheight="32" tilecount="504" columns="14">
+ <image source="MegaPixelArt32x32pxIcons_SpriteSheet.png" width="448" height="1152"/>
+</tileset>
