2a85ccb3302086ae4aa79a36c7850ae61f033868
fsoel <120105124+fsoel@users.noreply.github.com>
2023-02-11 16:45:35 +0000

    Deploying to gh-pages from master @ abe42f08d65812e9d21ed9a478a7e27040426ae1 🚀

--- assets/main-e2f2913b.js
+++
@@ -1,1 +0,0 @@
-(async()=>(await WA.onInit(),WA.chat.sendChatMessage(WA.room.id,"Bot"),WA.nav.goToRoom("../maps/map_test.tmj"),WA.chat.onChatMessage(a=>{a=="hilfe"&&WA.chat.sendChatMessage("helpmsg","Bot")})))();
