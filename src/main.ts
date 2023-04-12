e5d814d6f4c654740e8c5e0f55b29147758a59cb
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-04-12 21:40:53 +0200

    updated script

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import * as fs from 'fs';
 //import lib from "./lib";
 //import config from "./config";
 
@@ -9,15 +10,26 @@ console.log('Script started successfully');
 WA.onInit().then(() => {
     WA.chat.sendChatMessage('Willkommen!', 'Bot');
     WA.chat.onChatMessage(message => {
-        if(message == "fortnite") {
-            WA.nav.goToRoom('../maps/map_test.tmj');
+        // Ich habe hier Versucht ein Filter einzubauen es wurde aber noch nicht getestet. 
+        const data = fs.readFileSync('./data/badwords.json', 'utf8');
+        const obj = JSON.parse(data);
+        let words = obj.deutsch;
+        for (let i = 0; i < words.length; i++) {
+            if (message === words[i]) {
+                console.log('Bad Word Detected!')
+            }
+        }
+        words = obj.english;
+        for (let i = 0; i < words.length; i++) {
+            if (message === words[i]) {
+                console.log('Bad Word Detected!')
+            }
         }
     });
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags)
 
    
-
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
