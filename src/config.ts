3f86db287cda61b257bc365819ffd8e19c3380a3
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-05-16 14:02:18 +0200

    Upsarws

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+let config = {
+    main_map: "../maps/map.tmj",
+    test_map: "../maps/map_test.json", 
+}
+
+export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 /// <reference types="@workadventure/iframe-api-typings" />
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+//import * as fs from "fs";
+//import lib from "./lib";
+//import config from "./config";
 
 console.log('Script started successfully');
 
-
-// Waiting for the API to be ready
 WA.onInit().then(() => {
-    WA.nav.goToRoom('../Erdgeschoss.tmj');
+    WA.chat.sendChatMessage('Willkommen!', 'Bot');
+   
+    console.log('Scripting API ready');
+    console.log('Player tags: ', WA.player.tags)
+
+   
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
 }).catch(e => console.error(e));
 
 export {};
