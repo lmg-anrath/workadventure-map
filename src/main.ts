ddbd121bb709277ec94ec6f4b4e95c33692261fd
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-05-23 12:40:14 +0200

    moved files to maps and changed file paths

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ console.log('Script started successfully');
 // @ts-ignore
 WA.onInit().then(() => {
     // @ts-ignore
-    WA.nav.goToRoom('/_/global/maps.workadventure-lmg.de/test.tmj');
+    WA.nav.goToRoom('/_/global/maps.workadventure-lmg.de/maps/test.tmj');
 
     // Create a new website object
 
