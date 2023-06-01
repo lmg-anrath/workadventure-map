240e5b18e4b9cac2e260aa8941ddbdc65c4b7bf1
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-06-01 10:42:19 +0200

    added script to map

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,35 @@
-// @ts-ignore
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
-// @ts-ignore
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-// @ts-ignore
+//@ts-ignore
 import {Wait} from './common/functions';
+import * as Config from './configs/config.json';
 
 console.log('Script started successfully');
 
-// let currentPopup: any = undefined;
-
-// Waiting for the API to be ready
-// @ts-ignore
 WA.onInit().then(async () => {
-    // @ts-ignore
-    // WA.nav.goToRoom('/_/global/maps.workadventure-lmg.de/maps/rooms/forum.tmj');
-
-    // Create a new website object
-
-    // console.log('Scripting API ready');
-    // console.log('Player tags: ',WA.player.tags)
-
-    // WA.room.area.onEnter('clock').subscribe(() => {
-    //     const today = new Date();
-    //     const time = today.getHours() + ":" + today.getMinutes();
-    //     currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    // })
-
-    // WA.room.area.onLeave('clock').subscribe(closePopup)
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    
+    //@region: WA Boilerplate
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
-    // @ts-ignore
     }).catch(e => console.error(e));
-// @ts-ignore
+
+    //@region: Zones Event Handler
+    if (!Config) {
+        console.log("[ERROR] : Config not found.");
+        return;
+    }
+    console.log('json Config loaded');
+
+    //@region: Door-zone Event Handler 
+    Object.entries(Config.Zones.doors).forEach(([zone, targetRoom]) => {
+        console.log(zone, targetRoom);
+        WA.room.area.onEnter(zone).subscribe(() => {
+            WA.nav.goToRoom(targetRoom);
+        })
+    })
+
+
+
 }).catch(e => console.error(e));
 
-// function closePopup(){
-//     if (currentPopup !== undefined) {
-//         currentPopup.close();
-//         currentPopup = undefined;
-//     }
-// }
-
-export { };
-
+export {};
