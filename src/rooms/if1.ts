7fca99c417aa33c28f2381d2b4b1c5bd2a8c1491
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-06-01 10:51:09 +0200

    updated scripts

--- src/rooms/if1.ts
+++ src/rooms/if1.ts
@@ -1,3 +1,21 @@
+import * as Config from '../configs/if1.config.json';
+
+//@comment: Test-Code
+WA.onInit().then(async () => {
+
+    //@region: Zones Event Handler
+    if (!Config) {
+        console.log("[ERROR] : Config not found.");
+        return;
+    }
+    console.log('json Config loaded');
+
+    Object.entries(Config.Zones).forEach(([key, value]) => {
+        console.log(key, value);
+    })
+
+})
+
 console.log('if1.ts is running');
 
 export {};
