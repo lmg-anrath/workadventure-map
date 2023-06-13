0bcba146fa29bb5d09058f0e1e142a7e879f629c
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-06-13 08:53:07 +0200

    updated map

--- src/rooms/entrance.ts
+++ src/rooms/entrance.ts
@@ -1,1 +1,25 @@
-export {}
+import * as Config from '../configs/entrance.config.json';
+import {Global} from '../main';
+
+//@comment: Test-Code
+WA.onInit().then(async () => {
+
+    //@region: global script execution
+    Global()
+
+    //@region: Zones Event Handler
+    if (!Config) {
+        console.log("[ERROR] : Config not found.");
+        return;
+    }
+    console.log('json Config loaded');
+
+    Object.entries(Config.Zones).forEach(([key, value]) => {
+        console.log(key, value);
+    })
+
+})
+
+console.log('if1.ts is running');
+
+export {};
