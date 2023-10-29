83df4b8972e4d4140e57f3bf1d81e0f8020c3401
JH220 <admin@jh220.de>
2023-10-29 13:00:22 +0100

    Added dedicated entrance map
    Co-Authored-By: StanleyEitel <113137229+StanleyEitel@users.noreply.github.com>

--- src/rooms/entrance.ts
+++ src/rooms/entrance.ts
@@ -23,6 +23,39 @@ WA.onInit().then(async () => {
         })
     })
 
+
+	Object.entries(Config.Zones.ressources).forEach(([zone, targetURL]) => {
+        WA.room.area.onEnter(zone).subscribe(() => {
+            console.log("entered zone");
+			if (targetURL == "map") {
+				async () => {
+					WA.ui.modal.closeModal()
+					const pos = await WA.player.getPosition()
+					let mapOverviewPage = WA.ui.modal.openModal({
+						src: `https://www.workadventure-lmg.de/map?x=${pos.x}&y=${pos.y}&map=`,
+						allow: 'fullscreen',
+						title: 'Karte des LMG',
+						allowApi: true,
+						position: 'center',
+					})
+					console.log(`map overview ${mapOverviewPage}`)
+				}
+			}
+            WA.ui.modal.openModal({
+                title: "Haupteingang",
+                src: targetURL,
+                allow: "fullscreen",
+                allowApi: true,
+                position: "center",
+            })
+        })
+
+        WA.room.area.onLeave(zone).subscribe(() => {
+            console.log("left zone");
+            WA.ui.modal.closeModal();
+        })
+    })
+
 })
 
 console.log('entrance.ts is running');
---
+++ src/rooms/secretariat.ts
@@ -0,0 +1,30 @@
+import * as Config from '../configs/secretariat.config.json';
+import {Global} from '../main';
+
+//@comment: Test-Code
+WA.onInit().then(async () => {
+
+    //@region: global script execution
+    Global()
+
+    //@region: Zones Event Handler
+    if (!Config) {
+        console.log("[ERROR] : Config not found.");
+        return;
+    }
+    console.log('json Config loaded');
+
+    //@region: Door-zone Event Handler
+    Object.entries(Config.Zones.doors).forEach(([zone, targetRoom]) => {
+        console.log(zone, targetRoom);
+        WA.room.area.onEnter(zone).subscribe(() => {
+            console.log("entered zone");
+            WA.nav.goToRoom(targetRoom);
+        })
+    })
+
+})
+
+console.log('secretariat.ts is running');
+
+export {};
